a75940602987bc8a11045a88268d833ebc3bfdd9
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-02 01:34:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nowServing(line) {
   line.shift();
   if(line > 0) {
     return customer;
-  } else { return "There is nobody waiting to be served!";
+  } else { return ("There is nobody waiting to be served!");
   
 }
