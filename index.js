5c35f7d0bbb13315158ea67a68acf120b2be9908
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-02 01:41:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(line, name) {
   line.push(name);
-  return("Welcome, " + name + ". You are number " + (line.length + 1) + " in line.");
+  return("Welcome, " + name + ". You are number " + (line.length) + " in line.");
 }
 
 function nowServing(line) {
