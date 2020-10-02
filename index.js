79533f71a3e609edb7de14a6892faf5346af5bf1
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-02 01:34:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function takeANumber(line, name){
+function takeANumber(line, name) {
   return("Welcome, " + name + ". You are number " + (line.length + 1) + " in line.");
 }
 
 function nowServing(line) {
+  var customer = line[0];
+  line.shift();
+  if(line > 0) {
+    return customer;
+  } else { return "There is nobody waiting to be served!";
   
 }
