f714284f4031bbb4253ed5186cb26800644f69f5
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-02 01:39:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line) {
   } else {
     var people = "The line is currently:";
     for(let i = 0; i < line.length; i++) {
-      people += " " + (i + 1) + ". " + line[i] + ",");
+      people += " " + (i + 1) + ". " + line[i] + ",";
     }
     return people;
 }
